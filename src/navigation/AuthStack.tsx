0f930b9fa5468b026f52cc54f5646d0d90a32749
unknown <kingcrown20200@gmail.com>
2025-06-22 04:11:44 +0900

    0622

--- src/navigation/AuthStack.tsx
+++ src/navigation/AuthStack.tsx
@@ -16,8 +16,8 @@ const Stack = createNativeStackNavigator<AuthStackParamList>();
 export default function AuthStack() {
   const { user } = useContext(AuthContext);
   // user가 없거나 이메일 인증되지 않은 경우 VerifyEmail을 초기 화면으로
-  const initialRouteName: keyof AuthStackParamList = 'VerifyEmail';
-    // user && !user.emailVerified ? 'VerifyEmail' : 'SignIn';
+  const initialRouteName: keyof AuthStackParamList =
+    user && !user.emailVerified ? 'VerifyEmail' : 'SignIn';
 
   return (
     <Stack.Navigator
--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -21,8 +21,8 @@ export default function RootNavigator() {
   return (
     <NavigationContainer>
       {/* 로그인 전 또는 이메일 인증 미완료 시 AuthStack, 모두 완료 시 AppStack */}
-      {<AppStack />}
-      {/* {user && user.emailVerified ? <AppStack />:<AuthStack />} */}
+      {/* {<AppStack />} */}
+      {user && user.emailVerified ? <AppStack />:<AuthStack />}
     </NavigationContainer>
   );
 }
